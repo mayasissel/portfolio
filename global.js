d80b4cf2058f8d79c663a164a66d60aff60195ad
Maya Sissel <msissel@ucsd.edu>
2025-10-16 16:01:10 -0700

    finished steps 1-3

---
+++ global.js
@@ -0,0 +1,60 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// const navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// currentLink.classList.add('current');
+
+// if (currentLink) {
+//   // or if (currentLink !== undefined)
+//   currentLink.classList.add('current');
+// }
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/mayasissel/portfolio', title: 'GitHub' }
+];
+
+// Adjust base path based on environment
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                   // Local
+  : "/portfolio/";
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  // Highlight current page
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  // Open external links in new tab
+  if (a.host !== location.host) {
+  a.target = "_blank";
+  a.rel = "noopener noreferrer"; // (optional but recommended for security)
+}
+
+
+  nav.append(a);
+}
