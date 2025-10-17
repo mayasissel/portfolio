e6ec7ddad145722746423aadad48cd10f4a70154
Maya Sissel <msissel@ucsd.edu>
2025-10-16 17:18:17 -0700

    finished lab 3

--- global.js
+++ global.js
@@ -53,8 +53,54 @@ for (let p of pages) {
   if (a.host !== location.host) {
   a.target = "_blank";
   a.rel = "noopener noreferrer"; // (optional but recommended for security)
-}
-
+  }
 
   nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+let select = document.querySelector('.color-scheme select');
+
+function setColorScheme(value) {
+  document.documentElement.style.setProperty('color-scheme', value);
+  select.value = value;
+}
+
+if ("colorScheme" in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener('input', (event) => {
+  const value = event.target.value;
+  setColorScheme(value);
+  localStorage.colorScheme = value;
+});
+
+let form = document.querySelector("form");
+
+form?.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let data = new FormData(form);
+  let params = [];
+
+  for (let [name, value] of data) {
+    params.push(`${name}=${encodeURIComponent(value)}`);
+  }
+
+  let url = `${form.action}?${params.join("&")}`;
+  location.href = url;
+});
