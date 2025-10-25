16cf7df52528517cef4436258aba88cf7fc25b84
Maya Sissel <msissel@ucsd.edu>
2025-10-24 17:13:00 -0700

    finished step 1

--- global.js
+++ global.js
@@ -19,6 +19,19 @@ export async function fetchJSON(url) {
   }
 }
 
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  project.forEach(project => {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  });
+}
+
 let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
