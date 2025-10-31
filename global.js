c5708f008249b38b8d3a25876cfeb471941c995b
Maya Sissel <msissel@ucsd.edu>
2025-10-30 19:22:25 -0700

    finished step 1.3

--- global.js
+++ global.js
@@ -27,7 +27,10 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
+      <div class="project-info">
+        <p>${project.description}</p>
+        <p class="project-year">${project.year}</p>
+      </div>
     `;
     containerElement.appendChild(article);
   });
--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,12 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
 
 const projects = await fetchJSON('../lib/projects.json');
 
