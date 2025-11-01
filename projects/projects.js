1f34ad005f69872331325048a3a33ffdbbbcf870
Maya Sissel <msissel@ucsd.edu>
2025-10-31 17:00:57 -0700

    finished step 1

--- projects/projects.js
+++ projects/projects.js
@@ -1,18 +1,22 @@
 import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
-
 const projects = await fetchJSON('../lib/projects.json');
-
 const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `${projects.length} Projects`;
-
 const projectsContainer = document.querySelector('.projects');
-
 renderProjects(projects, projectsContainer, 'h2');
+
+
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcData.forEach((d, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arcGenerator(d))
+      .attr('fill', colors(idx))
+})
